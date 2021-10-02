b6fb043a1d5ea6c47f037d582e75d560081c1ac5
SenpaiXYZ <jhicer@gmail.com>
2021-10-02 22:47:52 +0800

    added metadata

--- app.py
+++ app.py
@@ -74,5 +74,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port='3000',debug=True)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
