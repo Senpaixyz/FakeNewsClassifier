623f99f644f6ba855d7b387d0f8b01c31b088c45
SenpaiXYZ <jhicer@gmail.com>
2021-10-15 16:27:10 +0800

    added images folder

--- app.py
+++ app.py
@@ -74,5 +74,6 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='3000',debug=True)
+    #app.run(host='0.0.0.0',port='3000',debug=True)
+    app.run(debug=True)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
